c988383167f08b5d8d5650d088e4f5916f2a8232
dianannperla <waafirmelyza@hotmail.com>
2025-10-04 18:22:52 +0100

    Updated file paths in content array for better coverage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx,html,svelte,vue,md,mdx}',
-    './public/**/*.{html}'
+    './public/**/*.{html,htm}'
   ],
   theme: {
     extend: {},
